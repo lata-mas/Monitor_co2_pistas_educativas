c9b251d0cf831ae54c5b9e9117b6270060842b08
Aztec Eagle Turbo <turbo@gendo-ikari.ier.unam.mx>
2021-07-07 19:44:26 -0500

    Fixed mistaken CREDENTIALS in arduino_secrets.h
    Vaya, por fin regresó el iZZi. Corrigiendo las CREDENCIALES
    de acceso al thingsboard.

--- sketchbook/co2-all-sensors/arduino_secrets.h
+++ sketchbook/co2-all-sensors/arduino_secrets.h
@@ -7,15 +7,15 @@
 #ifdef _ARDUINO_MQTT_CLIENT_H_
 #define SECRET_BROKER "iot.ier.unam.mx"
 #define SECRET_PORT 1883
-#define SECRET_DEVID "a12168c0-db50-11eb-897a-cd1c177f68f0"
-#define SECRET_TOKEN "moWNVYpt9hNWaZSKjsun"
+#define SECRET_DEVID "d147d250-d24a-11eb-897a-cd1c177f68f0"
+#define SECRET_TOKEN "nCsfwwiNH8JpXLmI2yuY"
 #define SECRET_TOPIC "v1/devices/me/telemetry"
 #endif
 
 #ifdef ArduinoHttpClient_h
 #define SECRET_SERVER "iot.ier.unam.mx"
 #define SECRET_PORT 8080
-#define SECRET_TOPIC "/api/v1/moWNVYpt9hNWaZSKjsun/telemetry"
+#define SECRET_TOPIC "/api/v1/nCsfwwiNH8JpXLmI2yuY/telemetry"
 #endif
 
 #endif
