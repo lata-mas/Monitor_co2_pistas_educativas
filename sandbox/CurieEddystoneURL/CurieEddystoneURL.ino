4f2779c2fdf9a8b700f3f3231bde8f3eb56f6e64
Aztec Eagle Turbo <hdcg@ier.unam.mx>
2021-08-20 22:59:25 -0500

    Intento de poner un BEACON con el URL del Sensor, pero no sirve sin instalar una apĺicación
    Aunque el BEACON funciona, requiere instalar una aplicación, así que realmente no es muy útil para el usuario final.
    En su lugar, el método WIFI-AP -> WIFI_STA es mucho más sencillo para inicializar los dispositivos.

--- sandbox/CurieEddystoneURL/CurieEddystoneURL.ino
+++ sandbox/CurieEddystoneURL/CurieEddystoneURL.ino
@@ -1,338 +1,127 @@
-/*
- * Eddystone URL for Arduino/Genuino 101.
- * 
- * This is an Intel Curie Eddystone-compatible Sketch
- * (https://github.com/google/eddystone).
- * 
- * The Eddystone BLE protocol is used by the Physical Web(tm) project
- * (https://github.com/google/physical-web).
- * The Physical Web project is about BLE beacons that broadcast URLs
- * so your users don't have to download an app just to interact with
- * your device.
- * 
- * Note: this version doesn't support Eddystone Config mode.
- * To change the URL, you need to modify MY_URL and reload the Arduino.
- * 
- * Parts required:
- *   Arduino/Genuino 101.  This Sketch runs only on Curie-based devices.
- *   No other hardware is required.
- * 
- * To Use:
- * - Setup your CurieBLE libraries to support Eddystone.
- *   Doing this may be a little complicated until the Eddystone support
- *   is added to the CurieBLE library.  Until then...
- *   There are two ways to do that:
- *   1) Pull code from https://github.com/bneedhamia/corelibs-arduino101/tree/support-eddystone-url
- *   or
- *   2) Replace CurieBLE/BLEPeripheral.* and CurieBLE/keywords.txt 
- *   with a modified version that supports Eddystone Advertising packets.
- *   See https://github.com/bneedhamia/CurieBLEServiceData
- *   once that's done...
- * - Set MY_URL to the url for the beacon you wish to broadcast.
- * - Download this Sketch to an Arduino/Genuino 101.
- * - Download a Physical Web app to your phone.
- *   For example, Google's Physical Web Android app is at
- *   https://play.google.com/store/apps/details?id=physical_web.org.physicalweb
- *   
- * Copyright (c) 2016 Bradford Needham, North Plains, Oregon, USA
- * @bneedhamia, https://www.needhamia.com
- * Licensed under the Apache 2.0 License, a copy of which
- * should have been included with this software.
- */
-
-/*
- * Note: as of this writing, the documentation of the CurieBLE library
- * is the library source, at (on Linux)
- * ~/.arduino15/packages/Intel/hardware/arc32/1.0.4/libraries/CurieBle/src
- * 
- * Version 1.0.4 of CurieBle doesn't support Eddystone-format
- * BLE Advertising packets, for 2 reasons:
- * - It uses the BLE code for Incomplete Service UUID list
- *   instead of Complete Service UUID list code required by Eddystone.
- * - It doesn't support Service Data, which is required by Eddystone.
- * 
- * Note: the name of the Curie BLE library changed between 1.0.4 and
- * the latest (as of March 10, 2016) version:
- * In 1.0.4, the name is CurieBle.h; in later versions, it's CurieBLE.h
- */
-#include <CurieBLE.h>        // BHN-Modified Curie-specific BLE Library
-
-/*
- * Pinout:
- *   PIN_BLINK = digital Output. The normal Arduino 101 LED.
- *     If everything is working, the LED blinks.
- *     If instead there is a startup error, the LED is solid.
- */
-const int PIN_BLINK = 13;        // Pin 13 is the on-board LED
-
-/*
- * The (unencoded) URL to put into the
- * Eddystone-URL beacon frame.
- * 
- * NOTE: the encoded length must be under 18 bytes long.
- * See initEddystoneUrlFrame().
+/* Copyright (c) 2018 pcbreflux. All Rights Reserved.
  *
- * NOTE: This Sketch supports only lower-case URLs.
- * See initEddystoneUrlFrame().
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>. *
  */
-const char* MY_URL = "http://www.intel.com";
-
+ 
 /*
- * The reported Tx Power value to put into the Eddystone-URL beacon frame.
- * 
- * From the Eddystone-URL Protocol specification
- * (https://github.com/google/eddystone/blob/master/eddystone-url/README.md#tx-power-level)
- * "the best way to determine the precise value
- * to put into this field is to measure the actual output
- * of your beacon from 1 meter away and then add 41dBm to that.
- * 41dBm is the signal loss that occurs over 1 meter."
- * 
- * I used the Nordic nRF Master Control Panel Android app
- * (https://play.google.com/store/apps/details?id=no.nordicsemi.android.mcp&hl=en)
- * to measure the beacon power at 1 meter.
- */
-const int8_t TX_POWER_DBM = (-70 + 41);
+   Create a BLE server that will send periodic eddystone frames.
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create advertising data
+   3. Start advertising.
+   4. wait
+   5. Stop advertising.
+   6. deep sleep
+   
+*/
+#include "sys/time.h"
 
-/*
- * Maximum number of bytes in an Eddystone-URL frame.
- * The Eddystone-URL frame contains
- * the frame type, Tx Power, Url Prefix, and up to 17 bytes of Url.
- * (The spec isn't completely clear.  That might be 18 rather than 17.)
- */
-const uint8_t MAX_URL_FRAME_LENGTH = 1 + 1 + 1 + 17;
+#include "BLEDevice.h"
+#include "BLEServer.h"
+#include "BLEUtils.h"
+#include "esp_sleep.h"
 
-/*
- * Eddystone-URL frame type
- */
-const uint8_t FRAME_TYPE_EDDYSTONE_URL = 0x10;
+#define GPIO_DEEP_SLEEP_DURATION     1  // sleep 4 seconds and then wake up
+RTC_DATA_ATTR static time_t last;        // remember last boot in RTC Memory
+RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
 
-/*
- * Eddystone-URL url prefix types
- * representing the starting characters of the URL.
- * 0x00 = http://www.
- * 0x01 = https://www.
- * 0x02 = http://
- * 0x03 = https://
- */
-const uint8_t URL_PREFIX_HTTP_WWW_DOT = 0x00;
-const uint8_t URL_PREFIX_HTTPS_WWW_DOT = 0x01;
-const uint8_t URL_PREFIX_HTTP_COLON_SLASH_SLASH = 0x02;
-const uint8_t URL_PREFIX_HTTPS_COLON_SLASH_SLASH = 0x03;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-/*
- * eddyService = Our BLE Eddystone Service.
- * See https://developer.bluetooth.org/gatt/services/Pages/ServicesHome.aspx 
- * and https://github.com/google/eddystone/blob/master/protocol-specification.md
- * NOTE: as of this writing, Eddystone doesn't seem to be part of the Standard BLE Services list.
- * 
- */
-BLEService eddyService("FEAA");
+uint8_t temprature_sens_read();
+//uint8_t g_phyFuns;
 
-BLEPeripheral ble;              // Root of our BLE Peripheral (server) capability
+#ifdef __cplusplus
+}
+#endif
 
-/*
- * The Service Data to Advertise.
- * See initEddystoneUrlFrame().
- * 
- * urlFrame[] = the Service Data for our Eddystone-URL frame.
- * urlFrameLength = the number of bytes in urlFrame[].
- */
-uint8_t urlFrame[MAX_URL_FRAME_LENGTH];
-uint8_t urlFrameLength;
-
-/*
- * If false after setup(), setup failed.
- */
-bool setupSucceeded;
-
-
-void setup() {  
-  Serial.begin(9600);
-
-  pinMode(PIN_BLINK, OUTPUT);
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+BLEAdvertising *pAdvertising;
+struct timeval now;
   
-  setupSucceeded = false;
-  digitalWrite(PIN_BLINK, HIGH);
-
-  if (!initEddystoneUrlFrame(TX_POWER_DBM, MY_URL)) {
-    return; // don't start advertising if the URL won't work.
-  }
-    
-  /*
-   * Set up Bluetooth Low Energy Advertising
-   * of the Eddystone-URL frame.
-   */
-  ble.setAdvertisedServiceUuid(eddyService.uuid());
-  ble.setAdvertisedServiceData(eddyService.uuid(), urlFrame, urlFrameLength);
+void setBeacon() {
+  char beacon_data[22];
+  uint16_t beconUUID = 0xFEAA;
+  uint16_t volt = 3300; // 3300mV = 3.3V
+  uint16_t temp = (uint16_t)((float)23.00);
+  uint32_t tmil = now.tv_sec*10;
+  uint8_t temp_farenheit;
+  float temp_celsius;
   
-  // Start Advertising our Eddystone URL.
-  ble.begin();
+  temp_farenheit= temprature_sens_read();
+  temp_celsius = ( temp_farenheit - 32 ) / 1.8;
+  temp = (uint16_t)(temp_celsius);
+  
+  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+  
+  oAdvertisementData.setFlags(0x06); // GENERAL_DISC_MODE 0x02 | BR_EDR_NOT_SUPPORTED 0x04
+  oAdvertisementData.setCompleteServices(BLEUUID(beconUUID));
 
-  setupSucceeded = true;
-  digitalWrite(PIN_BLINK, LOW);
+    beacon_data[0] = 0x10;  // Eddystone Frame Type (Eddystone-URL)
+    beacon_data[1] = 0x20;  // Beacons TX power at 0m
+    beacon_data[2] = 0x01;  // URL Scheme 'https://www.'
+    beacon_data[3] = 'i';  // URL add  1
+    beacon_data[4] = 'e';  // URL add  2
+    beacon_data[5] = 'r';  // URL add  3
+    beacon_data[6] = '.';  // URL add  4
+    beacon_data[7] = 'u';  // URL add  5
+    beacon_data[8] = 'n';  // URL add  6
+    beacon_data[9] = 'a';  // URL add  7
+    beacon_data[10] = 'm';  // URL add  8
+    beacon_data[11] = '.';  // URL add  9
+    beacon_data[12] = 'm';  // URL add 10
+    beacon_data[13] = 'x';  // URL add 11
+    beacon_data[14] = '/';  // URL add 12
+    beacon_data[15] =  0 ;  // URL add 13
+  
+  oAdvertisementData.setServiceData(BLEUUID(beconUUID), std::string(beacon_data, 16));
+  
+  pAdvertising->setScanResponseData(oAdvertisementData);
+
 }
 
+void setup() {
 
-unsigned long prevReportMillis = 0L;
+    
+  Serial.begin(115200);
+  gettimeofday(&now, NULL);
+
+  Serial.printf("start ESP32 %d\n",bootcount++);
+
+  Serial.printf("deep sleep (%lds since last reset, %lds since last boot)\n",now.tv_sec,now.tv_sec-last);
+
+  last = now.tv_sec;
+  
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+
+  pAdvertising = pServer->getAdvertising();
+  
+  setBeacon();
+   // Start advertising
+  pAdvertising->start();
+  Serial.println("Advertizing started...");
+  delay(100);
+  pAdvertising->stop();
+  Serial.printf("enter deep sleep\n");
+  esp_deep_sleep(1000000LL * GPIO_DEEP_SLEEP_DURATION);
+  Serial.printf("in deep sleep\n");
+}
 
 void loop() {
-
-  // If setup() failed, do nothing
-  if (!setupSucceeded) {
-    delay(1);
-    return;
-  }
-
-  unsigned long now = millis();
-
-  if ((now % 1000) < 100) {
-    digitalWrite(PIN_BLINK, HIGH);
-  } else {
-    digitalWrite(PIN_BLINK, LOW);
-  }
-
-  if ((long) (now - (prevReportMillis + 10000L))>= 0L) {
-    prevReportMillis = now;
-    //report();  // uncomment this line to see the completed advertising block.
-
-  }
-}
-
-
-/*
- * Prints the complete advertising block
- * that was assembled by CurieBLE.
- * For debugging.
- */
-void report() {
-  uint8_t len = ble.getAdvertisingLength();
-  uint8_t* pAdv = ble.getAdvertising();
-
-  Serial.print("Advertising block length: ");
-  Serial.println(len);
-  for (int i = 0; i < len; ++i) {
-    Serial.print("0x");
-    Serial.println(pAdv[i], HEX);
-  }
-}
-
-
-/*
- * Fills urlFrame and urlFrameLength
- * based on the given Transmit power and URL.
- * 
- * txPower = the transmit power of the Arduino 101 to report.
- * See TX_POWER_DBM above.
- * 
- * url = the URL to encode.  Likely a shortened URL
- * produced by an URL shortening service such as https://goo.gl/
- * 
- * Returns true if successful; false otherwise.
- * If the encoded URL is too long, use an URL shortening service
- * before passing the (shortened) URL to setServiceData().
- * 
- * NOTE: most of the code of this function is untested.
- */
-boolean initEddystoneUrlFrame(int8_t txPower, const char* url) {
-  urlFrameLength = 0;
-
-  // The frame starts with a type byte, then power byte.
-  urlFrame[urlFrameLength++] = FRAME_TYPE_EDDYSTONE_URL;
-  urlFrame[urlFrameLength++] = (uint8_t) txPower;
-
-  if (url == 0 || url[0] == '\0') {
-    return false;   // empty URL
-  }
-
-  const char *pNext = url;
-
-  /*
-   * the next byte of the frame is an URL prefix code.
-   * See URL_PREFIX_* above.
-   */
-  
-  if (strncmp("http", pNext, 4) != 0) {
-    return false;  // doesn't start with HTTP or HTTPS.
-  }
-  pNext += 4;
-  
-  bool isHttps = false; // that is, HTTP
-  if (*pNext == 's') {
-    pNext++;
-    isHttps = true;
-  }
-
-  if (strncmp("://", pNext, 3) != 0) {
-    return false; // malformed URL
-  }
-  pNext += 3;
-
-  urlFrame[urlFrameLength] = URL_PREFIX_HTTP_COLON_SLASH_SLASH;
-  if (isHttps) {
-    urlFrame[urlFrameLength] = URL_PREFIX_HTTPS_COLON_SLASH_SLASH;
-  }
-
-  if (strncmp("www.", pNext, 4) == 0) {
-    pNext += 4;
-
-    urlFrame[urlFrameLength] = URL_PREFIX_HTTP_WWW_DOT;
-    if (isHttps) {
-      urlFrame[urlFrameLength] = URL_PREFIX_HTTPS_WWW_DOT;
-    }
-  }
-  
-  urlFrameLength++;
-
-  // Encode the URL.
-
-  while (urlFrameLength < MAX_URL_FRAME_LENGTH && *pNext != '\0') {
-    if (strncmp(".com/", pNext, 5) == 0) {
-      pNext += 5;
-      urlFrame[urlFrameLength++] = 0x00;
-    } else if (strncmp(".org/", pNext, 5) == 0) {
-      pNext += 5;
-      urlFrame[urlFrameLength++] = 0x01;
-    } else if (strncmp(".edu/", pNext, 5) == 0) {
-      pNext += 5;
-      urlFrame[urlFrameLength++] = 0x02;
-    } else if (strncmp(".net/", pNext, 5) == 0) {
-      pNext += 5;
-      urlFrame[urlFrameLength++] = 0x03;
-    } else if (strncmp(".info/", pNext, 6) == 0) {
-      pNext += 6;
-      urlFrame[urlFrameLength++] = 0x04;
-    } else if (strncmp(".biz/", pNext, 5) == 0) {
-      pNext += 5;
-      urlFrame[urlFrameLength++] = 0x05;
-    } else if (strncmp(".gov/", pNext, 5) == 0) {
-      pNext += 5;
-      urlFrame[urlFrameLength++] = 0x06;
-    } else if (strncmp(".com", pNext, 4) == 0) {
-      pNext += 4;
-      urlFrame[urlFrameLength++] = 0x07;
-    } else if (strncmp(".org", pNext, 4) == 0) {
-      pNext += 4;
-      urlFrame[urlFrameLength++] = 0x08;
-    } else if (strncmp(".edu", pNext, 4) == 0) {
-      pNext += 4;
-      urlFrame[urlFrameLength++] = 0x09;
-    } else if (strncmp(".net", pNext, 4) == 0) {
-      pNext += 4;
-      urlFrame[urlFrameLength++] = 0x0A;
-    } else if (strncmp(".info", pNext, 5) == 0) {
-      pNext += 5;
-      urlFrame[urlFrameLength++] = 0x0B;
-    } else if (strncmp(".biz", pNext, 4) == 0) {
-      pNext += 4;
-      urlFrame[urlFrameLength++] = 0x0C;
-    } else if (strncmp(".gov", pNext, 4) == 0) {
-      pNext += 4;
-      urlFrame[urlFrameLength++] = 0x0D;
-    } else {
-      // It's not special.  Just copy the character
-      urlFrame[urlFrameLength++] = (uint8_t) *pNext++;
-    }
-  }
-
-  return true;
 }
